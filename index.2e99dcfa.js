4675cefe8b969e2aa9f51f2ceac2f65e9f5c97f2
Siarhei Tsishkavets <kenny.sergey@gmail.com>
2024-02-12 14:43:14 +0100

    make build

---
+++ index.2e99dcfa.js
@@ -0,0 +1,4 @@
+var e=document.getElementById("largeImg");// eslint-disable-next-line no-shadow
+function t(t){e.setAttribute("src",this.href),t.preventDefault()}document.querySelectorAll(".list-item__link").forEach(function(e){e.addEventListener("click",t)});//# sourceMappingURL=index.2e99dcfa.js.map
+
+//# sourceMappingURL=index.2e99dcfa.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const largeImg = document.getElementById("largeImg");
+// eslint-disable-next-line no-shadow
+function clickGallarySmallImg(event) {
+    largeImg.setAttribute("src", this.href);
+    event.preventDefault();
+}
+const galleryLinks = document.querySelectorAll(".list-item__link");
+galleryLinks.forEach((link)=>{
+    link.addEventListener("click", clickGallarySmallImg);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
